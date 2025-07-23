e80e87166a9620694cf77223afbcab9493ea11aa
plutonium <plutonium@chernobyl.localdomain>
2025-07-23 18:14:58 +0530

    added the struct config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,22 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
-    println!("Searching for {}", query); 
-    println!("In file {}", file_path);
+    let config = parse_config(&args);
+    
+    println!("Searching for {}", config.query); 
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("should be able to read the file");
     println!("{:#?}", dbg!(contents));
-
-    println!("{:#?}", dbg!(args));
-    println!("Hello, world!");
-    let path: &'static str = env!("PATH");
-    println!("the $PATH variable at the time of compiling was: {path}");
 }
 
-fn parse_config(args: &[String]) -> (&str,&str){
-    let query = &args[1];
-    let file_path = &args[2];
-    (query,file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config {query,file_path}
 }
